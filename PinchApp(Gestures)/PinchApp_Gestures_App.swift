2e946cb2f59f30d0de7542b743956ec6d94c2c6c
Mehmet Ergün <mrmehmetergun@gmail.com>
2023-02-23 23:48:52 -0500

    Initial Commit

---
+++ PinchApp(Gestures)/PinchApp_Gestures_App.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp_Gestures_App.swift
+//  PinchApp(Gestures)
+//
+//  Created by Mehmet Ergün on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp_Gestures_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
